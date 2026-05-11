ffad678eedf57a473e1d8f9fbe259c88fdc60844
spaceraptor1 <taproctor00@gmail.com>
2026-05-11 15:22:51 -0400

    Initial commit

---
+++ rackcopier_be/src/main/java/com/example/rackcopier_be/RackcopierBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.rackcopier_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RackcopierBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RackcopierBeApplication.class, args);
+	}
+
+}
---
+++ rackcopier_be/src/test/java/com/example/rackcopier_be/RackcopierBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rackcopier_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RackcopierBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
